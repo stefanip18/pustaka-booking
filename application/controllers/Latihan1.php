1e68bf70c3dc2c02f6611ddc87715c1b85fb8495
stefani <stefanipujiastuti@gmail.com>
2023-03-29 21:03:17 +0700

    first commit

---
+++ application/controllers/Latihan1.php
@@ -0,0 +1,17 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "Selamat Datang..selamt belajar Web programming";
+        //$this->load->view('view-latihan1');
+    }
+
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->model('Model_latihan1');
+        $hasil = $this->Model_latihan1->jumlah($n1, $n2);
+        echo "hasil penjumlahan dari " . $n1 ." + ". $n2 ." = ".$hasil;
+        //$this->load->view('view_latihan1', $data);
+    }
+}
---
+++ application/views/view-latihan.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Hallo kawan..yuk kita belajar web programming..!! <br>
+    nili 1 = <? = $n1; ?>
+    nili 1 = <? = $n1; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+    $n1. " + " . $n2 . " = " .$hasil; ?>
+
+
+</body>
+
+</html>
